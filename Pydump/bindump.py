fc4f02f0f76c1944b8e1aa93215fa0c5100bc530
Psideralis <psideralis@gmail.com>
2020-07-24 17:58:18 -0600

    First Commit on VSCode.

---
+++ Pydump/bindump.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Psideralis"
+__copyright__ = "Copyright 2020, Psideralis"
+__credits__ = ["Psideralis","Daniel Zamora"]
+__license__ = "GPL"
+__version__ = "0.0.01"
+__maintainer__ = "Daniel Zamora"
+__email__ = "psideralis@gmail.com"
+__status__ = "Development"
+
+
+import sys
+
+def bindump(filename):  
+        counter = 0
+        stream = []
+        flow = []
+        read = open(filename, "rb")
+        dump = open(filename[:-4]+".dmp","w+")
+        lines = read.readlines()
+        for line in lines:
+            for byte in line:
+               stream += [(bin(byte))]
+        for byte in stream:
+            flow += [byte[2:]]
+        stream = []           
+        for byte in flow:
+            if len(byte) != 8:
+                if byte == '0':
+                    stream += ['00000000']
+                else:
+                    stream += [byte.zfill(8)]
+            else:
+                stream += [byte]
+        for byte in stream:
+            counter += 1
+            if counter == 8:
+                dump.write(byte+'\n')
+                counter = 0
+            else:
+                dump.write(byte+'\t')
+            
+        read.close()
+        dump.close()
+
+if __name__ == "__main__":
+    rfile = sys.argv[1]
+    bindump(rfile)
